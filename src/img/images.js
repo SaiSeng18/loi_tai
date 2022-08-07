5e580d7b3a2d6e6d6bbb2716c0a996e8ad3460b5
Sai Seng <saisengmongli14@gmail.com>
2022-08-07 13:17:35 +0630

    pngggggg

--- src/img/images.js
+++ src/img/images.js
@@ -2,7 +2,7 @@ import img1 from './1.jpg';
 import img2 from './2.jpg';
 import img3 from './3.jpg';
 import img4 from './4.jpg';
-import loitai from './LOI TAI.png';
+import loitai from './Loi Tai.png';
 import logomark from './LT logomark.svg';
 import cart from './cart.svg';
 import menu from './menu.svg';
