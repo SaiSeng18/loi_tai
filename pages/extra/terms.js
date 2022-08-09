6673f0f8602772cb2156fb4718aa868287ab17c3
Sai Seng <saisengmongli14@gmail.com>
2022-08-09 22:43:23 +0630

    term

--- pages/extra/terms.js
+++ pages/extra/terms.js
@@ -3,17 +3,23 @@ import React from 'react'
 const terms = () => {
   return (
 	<div className="body terms">
+    <br />
     Payment
+    <br />
     <br />
     Cash on delivery (or) any method of online payment
     <br />
     <br />
+    <br />
     Shipping and Delivery
+    <br />
     <br />
     We will deliver your items within 3-5 days once you have confirmed your order, provide a correct address we can deliver to. Once we have shipped your order, we will send you a digital receipt through the E-mail you have provided us.
     <br />
     <br />
+    <br />
     Returns and Cancellations
+    <br />
     <br />
     All sales are final unless there was an issue with wrong sizes or damage of goods.
     <br />
@@ -21,6 +27,12 @@ const terms = () => {
     <br />
     <br />
     loitaiofficial@gmail.com
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    *If an item is not taken within one-week time, it will be prioritized to the recent customer*
   </div>
   )
 }
